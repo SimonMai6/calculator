629951aa751cd10f57aeea4aed61a41cffa8ada4
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-06-27 16:36:57 -0700

    Add css html and js and basic form for calculator
    Created the bare bones skeleton for the
    calculator. None of the functions and buttons
    work yet.

---
+++ index.js
@@ -0,0 +1,38 @@
+let firstNum = 0;
+let operator = "";
+let secondNum = 0;
+
+
+
+
+function add(numOne, numTwo){
+    return numOne + numTwo;
+}
+
+
+function subtract(numOne, numTwo){
+    return numOne - numTwo;
+}
+
+
+function multiply(numOne, numTwo){
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo){
+    return numOne, numTwo;
+}
+
+function operate(numOne, numTwo, operate){
+    if(operate === "*"){
+        multiply(numOne, numTwo)
+    }
+}
+
+function display(){
+    
+}
+
+document.getElementById
+
+
