1fd128eea25283318bce3e4dbf37e12f3b648a5d
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-07-01 13:47:28 -0700

    Put character limit to the display
    Needed to put a character limit so that the
    numbers did not overflow from the display.

--- index.js
+++ index.js
@@ -4,86 +4,100 @@ let secondNum = "";
 
 const button = document.querySelectorAll("button");
 const displayContent = document.querySelector(".display");
-const operatorArr = ["+", "-", "×", "÷"]
+const operatorArr = ["+", "-", "×", "÷"];
 
 
-function add(numOne, numTwo){
-    return numOne + numTwo;
+function add (numOne, numTwo) {
+    return Number(numOne) + Number(numTwo);
 }
 
 
-function subtract(numOne, numTwo){
+function subtract (numOne, numTwo) {
     return numOne - numTwo;
 }
 
 
-function multiply(numOne, numTwo){
+function multiply (numOne, numTwo) {
     return numOne * numTwo;
 }
 
-function divide(numOne, numTwo){
-    return numOne, numTwo;
+function divide (numOne, numTwo) {
+    return numOne/numTwo;
 }
 
-function operate(numOne, numTwo, operate){
-    if(operate === "×"){
-        multiply(numOne, numTwo);
+function operate (numOne, numTwo, operate) {
+    switch (operate) {
+        case "+":
+            return add(numOne, numTwo);
+        case "-":
+            return subtract(numOne, numTwo);
+        case "×":
+            return multiply(numOne, numTwo);
+        case "÷":
+            return divide(numOne, numTwo);
+
     }
+
 }
 
-function display(contents){
+function display (contents) {
     displayContent.textContent = contents;
 
 }
 
-function clearDisplay(){
+function clearDisplay () {
     firstNum = "";
     operator = "";
     secondNum = "";
     display("");
 }
 
-function isOperator(operator){
-    if(operatorArr.includes(operator)){
-        return true;
-    }
-    else{
-        return false;
-    }
+function isOperator (operator) {
+    return operatorArr.includes(operator);
+}
+
+function isWithinLimit (wordLength) {
+    return wordLength < 15;
 }
 
 
-button.forEach(element => {
-    element.addEventListener(("click"), () =>{
-        if(element.textContent === "AC"){
-                clearDisplay();
+function addToDisplay (element) {
+    if (!isOperator(operator)) {
+        if (isWithinLimit(firstNum.length)) {
+            firstNum += element.textContent;
+            console.log(element.textContent);
+            display(firstNum);
         }
-        else if(isOperator(element.textContent)){
+    }
+    else {
+        if (isWithinLimit(secondNum.length)) {
+            secondNum += element.textContent;
+            console.log(element.textContent);
+            display(secondNum);
+        }
+    }
+}
+
+button.forEach( (element) => {
+    element.addEventListener( ("click"), () =>{
+        if (element.textContent === "AC") {
+            clearDisplay();
+        }
+        else if (isOperator(element.textContent)) {
             operator = element.textContent;
             display("");
         }
-        if(firstNum.length < 15){
-            
-
-            if(isOperator(operator)){
-                secondNum += element.textContent;
-                console.log(element.textContent);
-                display(secondNum);
-            }
-
-            else{
-                firstNum += element.textContent;
-                console.log(firstNum.length <= 15);
-                display(firstNum);
-            }
+        else if (element.textContent === "=") {
+            let sum = operate(firstNum, secondNum, operator);
+            clearDisplay();
+            display(sum);
+        }
+        else {
+            addToDisplay(element);
         }
         
-        else{
 
-        }
-
-
-    })
+    });
 });
 
 
